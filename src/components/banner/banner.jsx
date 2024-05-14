49e1eadf6dc989d814bfaa3fcd529bc18773b281
Behzad <dr.ben.r.1985@gmail.com>
2024-05-14 20:48:14 +0100

    font changed

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -7,14 +7,14 @@ const HeaderBanner = () => {
             <Container>
                 <Row className="justify-content-center">
                     <Col lg="10" md="10" className="align-self-center text-center">
-                        <h2 className="title" data-aos="fade-down">Acbcooling</h2>
-                         <h3 className='logo-title text-white'  data-aos="fade-down">
+                        <h3 className="title" data-aos="fade-down">Acbcooling</h3>
+                         <h4 className='logo-title text-white'  data-aos="fade-down">
                             Refrigeration & Air Condition <br /> Service, Repair & Installation
-                            </h3>
+                            </h4>
                             <br />
-                        <h4 className="subtitle font-light" data-aos="fade-up">
+                        <h5 className="subtitle font-light" data-aos="fade-up">
                         Acbcooling is a HVAC contractor and procurement company specialist for Air conditioning and Refrigeration. Coldskill designs, installs, and maintains advanced cooling systems to help people do better work and live better lives. 
-                        </h4>
+                        </h5>
                         <a href="tel:07737690300" className="btn btn-info-gradiant m-r-20 btn-md m-t-30 font-20" data-aos="fade-up"><i className="fa fa-phone"></i> &nbsp; Call Us</a>
                     </Col>
                 </Row>
