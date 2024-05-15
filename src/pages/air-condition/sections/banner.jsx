152f98c0c7be9db2283f37c077b160dabccac8af
Behzad <dr.ben.r.1985@gmail.com>
2024-05-15 23:24:49 +0100

    adding content for the pages

--- src/pages/air-condition/sections/banner.jsx
+++ src/pages/air-condition/sections/banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
                     <Col lg="10" md="10" className="align-self-center text-center">
                         <h1 className="title" data-aos="fade-down">Air Condition</h1>
                         <h4 className="subtitle font-light" data-aos="fade-up">
-                            Lorem ipsum doller sit amet dummy text
+                        Experience year-round comfort with our HVAC website's air conditioning solutions. From energy-efficient units to innovative climate control systems, we ensure your indoor environment stays cool and comfortable.
                         </h4>
                         <a href="tel:07376942333" className="btn btn-info-gradiant m-r-20 btn-md m-t-30 font-20" data-aos="fade-up"><i className="fa fa-phone"></i> &nbsp; Call Us</a>
                     </Col>
--- src/pages/refrigeration/sections/banner.jsx
+++ src/pages/refrigeration/sections/banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
                     <Col lg="10" md="10" className="align-self-center text-center">
                         <h1 className="title" data-aos="fade-down">Refrigeration Systems</h1>
                         <h4 className="subtitle font-light" data-aos="fade-up">
-                            Lorem ipsum doller sit amet dummy text
+                        Explore reliable refrigeration solutions on our HVAC website, designed to preserve freshness and optimize storage. From commercial-grade units to specialized systems, we offer efficient cooling solutions tailored to your needs
                         </h4>
                         <a href="tel:07376942333" className="btn btn-info-gradiant m-r-20 btn-md m-t-30 font-20" data-aos="fade-up"><i className="fa fa-phone"></i> &nbsp; Call Us</a>
                     </Col>
